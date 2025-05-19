97cad381f8f56530d674cd95cfb6ad969e1378ec
Bellet2002 <ella.fagerstrom@hotmail.com>
2025-05-19 12:09:35 +0200

    Node blir en inre klass

--- prog2-inlupp-template/gui/src/main/java/se/su/inlupp/Gui.java
+++ prog2-inlupp-template/gui/src/main/java/se/su/inlupp/Gui.java
@@ -28,6 +28,34 @@ import javafx.stage.Stage;
 
 public class Gui extends Application {
 
+  class Node {
+    private String name;
+    private double yCoordinate;
+    private double xCoordinate;
+
+    public Node(String name, double x, double y) {
+        this.name = name;
+        this.yCoordinate = y;
+        this.xCoordinate = x;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getyCoordinate() {
+        return yCoordinate;
+    }
+
+    public double getxCoordinate() {
+        return xCoordinate;
+    }
+
+    public String toString() {
+        return name;
+    }
+  }
+
   private Stage stage;
   //Meny-knappar
   private MenuBar menuBar;
